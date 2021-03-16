339b7e7118312df4816c0c6ce0f6e963b7317889
Shaginyan Artur <arturexp@gmail.com>
2021-03-17 02:28:10 +0700

    VFX effect

--- Glitch Garden/Assets/Scripts/Health.cs
+++ Glitch Garden/Assets/Scripts/Health.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] float health = 100;
+    [SerializeField] GameObject deathVFX;
 
     public void DealDamage(float damage)
     {
         health -= damage;
         if (health <= 0)
         {
+            TrigerDeathVFX();
             Destroy(gameObject);
         }
+    }
+
+    public void TrigerDeathVFX()
+    {
+        if (!deathVFX) { return; }
+        GameObject deathVFXobject = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        Destroy(deathVFXobject, 2f);
     }
 }
