9e5168036779fd5ff165aedf2918d2c5bd5aef42
Shaginyan Artur <arturexp@gmail.com>
2021-03-16 20:07:08 +0700

    lizard walk

--- Glitch Garden/Assets/Scripts/Attacker.cs
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
+    [SerializeField] [Range(0f, 5f)] float walkSpeed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Attacker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
     }
 }
