25a1c89f357355bcb0877e1f4e57fab3086f57cf
Shaginyan Artur <arturexp@gmail.com>
2021-03-18 00:47:57 +0700

    resourse system

---
+++ Glitch Garden/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+
+    [SerializeField] int stars = 100;
+    Text starText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        starText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+    private void UpdateDisplay() {
+
+        starText.text = stars.ToString();
+        }
+
+    public void AddStars(int count) {
+        
+        stars += count;
+        UpdateDisplay();
+        }
+
+    public void SpendingStars(int count) {
+        
+        if (stars >= count) {
+
+            stars -= count;
+            UpdateDisplay();
+            }
+
+        }
+
+}
