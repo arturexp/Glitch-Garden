b9c10ed4e5cf64b007372f481c881adc739a71ac
Shaginyan Artur <arturexp@gmail.com>
2021-03-18 02:21:20 +0700

    star anim

--- Glitch Garden/Assets/Scripts/Defender.cs
+++ Glitch Garden/Assets/Scripts/Defender.cs
@@ -2,7 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Defender : MonoBehaviour
-{
+public class Defender: MonoBehaviour {
     [SerializeField] int starCost = 100;
-}
+
+    public void AddStars(int amount) {
+
+        FindObjectOfType<StarDisplay>().AddStars(amount);
+
+        }
+    }
