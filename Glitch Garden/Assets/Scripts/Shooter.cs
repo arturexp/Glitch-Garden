43c43d86024fc773f01e755af5a7a0ebfa79ca1f
Shaginyan Artur <arturexp@gmail.com>
2021-03-18 18:12:56 +0700

    axe projectile

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -7,20 +7,22 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
     AttackerSpawner myLaneSpawner;
+    Animator animator;
 
     private void Start() {
 
         SetLaneSpawner();
+        animator = GetComponent<Animator>();
     }
 
 
     private void Update() {
 
         if(IsAttackerInLane()) {
-            Debug.Log("shoot");
+            animator.SetBool("IsAttacking", true);
         }
         else {
-            Debug.Log("sit and wait");
+            animator.SetBool("IsAttacking", false);
         }
     }
 
